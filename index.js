0f9bd164c4a0d4603799a67aef802d8f3c050182
Shahfayaz Khan <shahfayaz.pk93@gmail.com>
2023-05-04 15:03:56 +0500

    changes

---
+++ index.js
@@ -0,0 +1,12 @@
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('public'));
+
+server.listen(PORT, function(){
+    console.log('Chat server running...!');
+});
